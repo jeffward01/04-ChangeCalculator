19832bc87a2dde53007bc8be0c3ea35ede2fea8b
Jeff-Ward <jeffwardcoding@gmail.com>
2015-09-24 14:13:25 -0700

    Building Program

---
+++ 04-Change Calculator/JeffToolBox.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Change_Calculator
+{
+    class JeffToolBox
+    {
+        public static decimal StringtoDecimal(string myString)
+        {
+
+
+            decimal myDecimal = Convert.ToDecimal(myString);
+
+            return myDecimal;
+        }
+
+        public static string DecimalToDollar(decimal myDecimal)
+        {
+            string myString = myDecimal.ToString();
+
+            myString = "$" + myString;
+            return myString;
+
+        }
+
+        public static string RectangleMath(decimal width, decimal length, decimal cost)
+        {
+            decimal Totalcost = width * length;
+            Totalcost = Totalcost * cost;
+            string finalCost = DecimalToDollar(Totalcost);
+            return finalCost;
+        }
+
+        public static string RectangleSqFt(decimal width, decimal length)
+        {
+            decimal sqft = width * length;
+            string totalSqFt = sqft.ToString();
+
+            return totalSqFt;
+        }
+
+        public static string testIntergersOnly(string myString)
+        {
+            while (true)
+            {
+                try
+                {
+
+
+                }
+
+                catch
+                {
+
+
+                }
+
+
+            }
+
+        }
+
+        public bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static string RemoveLetters(string myString)
+        {
+            string newString = Regex.Replace(myString, "[A-Za-z ]", "");
+
+            return newString;
+        }
+        public static bool hasLetters(string myString)
+        {
+            foreach (var i in myString)
+            {
+                if ((i < '0' || i > '9'))
+                {
+                    return false;
+
+                }
+            }
+
+            return true;
+
+
+        }
+
+        public static string RemoveSpecialCharacters(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static void WriteToConsole<T>(List<string> collection)
+        {
+            WriteToConsole<T>(collection, "\t");
+        }
+
+        public static void WriteToConsole<T>(List<string> collection, string delimiter)
+        {
+            int count = collection.Count();
+            for (int i = 0; i < count; ++i)
+            {
+                Console.Write("{0}{1}", collection[i].ToString(), delimiter);
+            }
+            Console.WriteLine();
+        }
+        public static string ListToString(List<string> collection)
+        {
+            string myString = string.Join(",", collection.ToArray());
+
+            return myString;
+        }
+
+
+
+    }//End class
+
+
+
+
+} // End NameSpace
+
--- 04-Change Calculator/Program.cs
+++ 04-Change Calculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,89 @@ namespace _04_Change_Calculator
 {
     class Program
     {
+        //Varibles for Program
+        static string Line ="----------------------- \n \n \n";
+        decimal NumOf50s, NumOf20s, NumOf10s, NumOf5s, NumOf2s, NumOf1s, 
+            NumOfQuarters, NumOfDimes, NumOfNickels, NumOfPennies;
+
+        static List<string> Transactions = new List<string>()
+        {
+            "Your Transactions",
+            Line,
+
+        };
+
+        static void DetermineChange(decimal SaleAmout)
+        {
+
+
+
+        }
+
+        static decimal TotalRegisterCash = 100.00;
+
+        static void OutputCash(decimal ChangeAmount)
+        {
+            TotalRegisterCash = TotalRegisterCash - ChangeAmount;
+
+        }
+
+        static void InputCash(decimal SaleAmount)
+        {
+
+            TotalRegisterCash = TotalRegisterCash + SaleAmount;
+
+        }
+
+        static void SaveTransactionsFile(List<string> myList)
+        {
+            if (Directory.Exists("C:\\ChangeCalculator\\Transactions"))
+            {
+                Directory.CreateDirectory("C:\\ChangeCalculator\\Transactions");
+            }
+
+            string contents = JeffToolBox.ListToString(myList);
+          
+            File.WriteAllText("C:\\ChangeCalculator\\Transactions", (contents));
+
+            string fileContents = File.ReadAllText("C:\\ChangeCalculator\\Transactions");
+
+            Console.WriteLine(Line+"Save file created Here: " + "C:\\ChangeCalculator\\Transactions \n \n " +
+                "Here are your file contents:  \n \n "+fileContents);
+       
+        }
+
+        public static void ProgramIntro()
+        {
+            Console.WriteLine(Line);
+            Console.WriteLine("Welcome to Cash Register 1.0");
+            Console.WriteLine("Press Enter to Begin");
+            Console.ReadLine();
+            Console.Clear();
+        }
+      
+
+
+
         static void Main(string[] args)
         {
+            try
+            {
+                ProgramIntro();
+
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(Line+ "An error has occured.. Please contact technical support immediatly.");
+                Console.ReadLine();
+                
+            }
+
+
+
         }
     }
 }
